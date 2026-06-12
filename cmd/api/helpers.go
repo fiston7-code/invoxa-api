96b47167ff2eb50109de7f49589b743b66946073
fiston <kalambayifiston7@gmail.com>
2026-06-12 13:10:18 +0100

    feat: add writeJSON helper for structured API responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -17,4 +18,34 @@ func (app *application) readIDParam(r *http.Request) (int, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+// Define a writeJSON() helper for sending responses. This takes the destination
+// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
+// headers map containing any additional HTTP headers we want to include in the response.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// Encode the data to JSON, returning the error if there was one.
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// Append a newline to make it easier to view in terminal applications.
+	js = append(js, '\n')
+	// At this point, we know that we won't encounter any more errors before writing the
+	// response, so it's safe to add any headers that we want to include. We loop
+	// through the headers map (which behind the scenes has the type map[string][]string)
+	// and add all the header keys and values to the http.ResponseWriter's header map.
+	// Note that it's OK if the provided headers map is nil. Go doesn't throw an error
+	// if you try to range over (or generally, read from) a nil map.
+	for key, values := range headers {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	// Add the "Content-Type: application/json" header, then write the status code and
+	// JSON response.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
